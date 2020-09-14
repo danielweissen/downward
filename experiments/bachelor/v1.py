d37c1bfb8fc964a68a65a44d2091378dec23679b
Thomas Keller <tho.keller@unibas.ch>
2020-09-14 16:44:01 +0200

    Comment parse_again_step

--- experiments/bachelor/v1.py
+++ experiments/bachelor/v1.py
@@ -40,15 +40,28 @@ exp = IssueExperiment(
 )
 exp.add_suite(BENCHMARKS_DIR, SUITE)
 
+#exp.add_parse_again_step()
+
 exp.add_parser(exp.EXITCODE_PARSER)
 exp.add_parser(exp.SINGLE_SEARCH_PARSER)
 exp.add_parser(exp.PLANNER_PARSER)
+exp.add_parser('custom-parser.py')
 
 exp.add_step('build', exp.build)
 exp.add_step('start', exp.start_runs)
 exp.add_fetcher(name='fetch')
 
-exp.add_absolute_report_step()
+ATTRIBUTES = exp.DEFAULT_TABLE_ATTRIBUTES + [
+    "perc_0_adjust",
+    "perc_1_adjust",
+    "perc_2_adjust",
+    "perc_overconsistent",
+    "perc_underconsistent",
+    "avg_num_diff_vars",
+    "var_num_diff_vars",
+]
+
+exp.add_absolute_report_step(attributes=ATTRIBUTES)
 #exp.add_comparison_table_step()
 #exp.add_scatter_plot_step(relative=True, attributes=["search_time", "total_time"])
 
